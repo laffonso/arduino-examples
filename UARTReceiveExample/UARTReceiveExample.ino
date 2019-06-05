d36248bde5aa996be4517719db7377eb47ccf5b1
laffonso <laffonso.lfta@gmail.com>
2019-06-05 10:47:50 -0300

    update variables

--- UARTReceiveExample/UARTReceiveExample.ino
+++ UARTReceiveExample/UARTReceiveExample.ino
@@ -1,9 +1,12 @@
-while(Uno.available() > 0){  //enquanto a porta serial estiver recebendo algo
-    caractere = Uno.read();    //coloca o próximo caractere na variável caractere
+String texto;
+char caractere;
+
+while(Serial.available() > 0){  //enquanto a porta serial estiver recebendo algo
+    caractere = Serial.read();    //coloca o próximo caractere na variável caractere
     if(caractere != '\n'){     //Se a variável não for um final de linha,
-      aux += caractere;    //Concatena o caractere na string url.
+      texto += caractere;    //Concatena o caractere na string url.
     }
     else                                //Caso o caractere for final de linha continuar a execução do programa para envio ao banco de dados 
     {
-      
+      Serial.print(texto)
     }
